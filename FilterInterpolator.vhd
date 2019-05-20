2844a35b5cfaa1ca56a9ef199cca1f08e1f02f30
Anders <akblabla@gmail.com>
2019-05-20 11:07:47 +0200

    fixed interpolator interface

--- FilterInterpolator.vhd
+++ FilterInterpolator.vhd
@@ -8,8 +8,9 @@ entity FilterInterpolator is
 	port(clk : in std_logic;
 		reset : in std_logic;
 		inputFilters : in fir_filter_array(1 downto 0);
+		delays : in signed8_array(1 downto 0);
 		outputFilter : out fir_filter;
-		weights: in signed32_array(1 downto 0));
+		weights: in signed32);
 end entity;
 
 architecture default of FilterInterpolator is
@@ -17,7 +18,11 @@ begin
 	interpolate: process(clk)
 	begin
 		for i in 0 to 255 loop
-			outputFilter(i)<= resize(shift_right(inputFilters(0)(i)*weights(0)+inputFilters(1)(i)*weights(1),31),32);
+			outputFilter(i)<= resize(
+				shift_right(
+					inputFilters(0)(i)*weights+inputFilters(1)(i)*(to_signed(2147483647,32)-weights)
+				,31)
+			,32);
 		end loop;
 	end process interpolate;
 
--- SorroundSoundTb.vhd
+++ SorroundSoundTb.vhd
@@ -11,11 +11,17 @@ architecture default of SorroundSoundTb is
 signal clk, reset : std_logic := '0';
 constant period : time := 20.83 us;
 signal inputFilters : fir_filter_array(1 downto 0);
+signal delays : signed8_array(1 downto 0);
 signal outputFilter : fir_filter;
-signal weights: signed32_array(1 downto 0);
+signal weights: signed32;
 begin
-	FI : entity work.FilterInterpolator port map(clk=>clk, reset=>reset, inputFilters=>inputFilters,outputFilter=>outputFilter,weights=>weights);
-	FI : entity work.FilterInterpolator port map(clk=>clk, reset=>reset, inputFilters=>inputFilters,outputFilter=>outputFilter,weights=>weights);
+	FI : entity work.FilterInterpolator port map(
+		clk=>clk,
+		reset=>reset,
+		inputFilters=>inputFilters,
+		delays=>delays,
+		outputFilter=>outputFilter,
+		weights=>weights);
 	process
 	begin
 		clk<=not clk;
@@ -29,8 +35,7 @@ begin
 			inputFilters(0)(i)<=to_signed(i*10,32);
 			inputFilters(1)(i)<=to_signed(1000,32);
 		end loop;
-		weights(0)<=to_signed(1073741824,32);
-		weights(1)<=to_signed(1073741824,32);
+		weights<=to_signed(1073741824,32);
 		wait for period;
 		assert outputFilter(0)= to_signed(500,32)
 			report "Filters are not weighted proper"
