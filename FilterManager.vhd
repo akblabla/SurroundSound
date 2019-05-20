267847654add3c818daed4bf7aae5d6c42320a1c
TheEpicZelda <mub1997@hotmail.com>
2019-05-20 11:08:19 +0200

    FilterManager entity

--- FilterManager.vhd
+++ FilterManager.vhd
@@ -1,3 +1,12 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+
+entity FilterManager is
+  port(clk       : in  std_logic;
+       reset     : in  std_logic;
+		 direction : in  unsigned(8 downto 0);
+		 weight    : out signed32;
+		 filters   : out fir_filter_array(1 downto 0);
+		 delays    : out signed8_array(1 downto 0);
+end entity;
