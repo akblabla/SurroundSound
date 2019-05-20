de14092316be09b55fa2a8c97bcb61d6f7636f13
Anders <akblabla@gmail.com>
2019-05-20 11:51:00 +0200

    fixed interpolator

--- FilterManager.vhd
+++ FilterManager.vhd
@@ -1,6 +1,8 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+library work;
+use work.FilterTypes.all;
 
 entity FilterManager is
   port(clk       : in  std_logic;
@@ -8,5 +10,5 @@ entity FilterManager is
 		 direction : in  unsigned(8 downto 0);
 		 weight    : out signed32;
 		 filters   : out fir_filter_array(1 downto 0);
-		 delays    : out signed8_array(1 downto 0);
+		 delays    : out signed8_array(1 downto 0));
 end entity;
