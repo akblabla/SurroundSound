38bfd75eb0812ea879ef797b76e7cd43ba7c7846
Anders <akblabla@gmail.com>
2019-05-29 14:02:38 +0200

    lavet første udgave af iis2bytestream og bytestream2iis

--- IIS2ByteStream.vhd
+++ IIS2ByteStream.vhd
@@ -4,32 +4,47 @@ use IEEE.numeric_std.all;
 library work;
 use work.FilterTypes.all;
 
+
 entity IIS2ByteStream is
   port(reset      : in  std_logic;
 		 bitclk           : in  std_logic;
 		 adcdat           : in  std_logic;
-		 dacdat           : out std_logic                     := '0';
 		 adclrck          : in  std_logic;
-		 daclrck          : in  std_logic;
-		 byteStream : out signed32);
+		 byteStreamLeft   : out signed32;
+		 byteStreamRight  : out signed32);
 end entity;
 
 architecture stream of IIS2ByteStream is
 
 begin
+
 	process
-	variable bitcount            : integer range 0 to 23         := 0;
-	variable st_source_data_temp : std_logic_vector(23 downto 0) := (others => '0');
+	constant IISbitResolution : integer := 24;
+	variable adclrckPrev            : std_logic := '0';
+	variable bitcount            : integer range 0 to IISbitResolution         := 0;
+	variable source_data_temp : std_logic_vector(IISbitResolution-1 downto 0) := (others => '0');
 	begin
 		if reset = '0' then
 			--Set IISdata 0
-			byteStream <= to_signed(0,32);
+			byteStreamLeft <= to_signed(0,32);
+			byteStreamRight <= to_signed(0,32);
 		elsif rising_edge(bitclk) then
---			if adclrck = '0'
---				
---				bitcount := bitcount+1;
---			end if;
---
+			if adclrckPrev = not adclrck then
+				bitcount := 0;
+				if adclrck = '0' then
+					byteStreamLeft <= resize(signed(source_data_temp),32);
+				else
+					byteStreamRight <= resize(signed(source_data_temp),32);				
+				end if;
+			elsif bitcount<IISbitResolution then
+				source_data_temp(bitcount) := adcdat;
+				bitcount := bitcount+1;
+			end if;
+			adclrckPrev := adclrck;
+			
+		end if;
+
+		
 --		end if;
 --			wait until adclrck = '0';
 --			wait for bitperiod;
@@ -42,6 +57,6 @@ begin
 --			ast_source_valid <= '0';
 --			ast_source_data  <= st_source_data_temp;
 --			ast_source_valid <= '1';
-		end if;
+
 	end process;
 end stream;
