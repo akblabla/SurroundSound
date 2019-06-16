5f27d237cefd050920183c83adbdbf44cea44cd4
TheEpicZelda <mub1997@hotmail.com>
2019-06-16 12:50:45 +0200

    Test

--- ByteStream2IIS.vhd
+++ ByteStream2IIS.vhd
@@ -26,26 +26,23 @@ begin
 	variable dest_data_temp : std_logic_vector(IISbitResolution-1 downto 0) := (others => '0');
   begin
     if reset = '0' then
-	   --Set IISdata 0
-
-	elsif rising_edge(bitclk) then
-
-	   --Put data from IISdata to byteStream
-
-	elsif rising_edge(clk) then
-		if reset = '0' then
-			--Set IISdata 0
-			dacdat <= '0';
-		elsif rising_edge(bitclk) then
-			if daclrckPrev = not daclrck then
-				bitcount := 0;
-				if daclrck = '0' then
-					dest_data_temp := std_logic_vector(byteStreamLeft)(IISbitResolution-1 downto 0);
-				else
-					dest_data_temp := std_logic_vector(byteStreamRight)(IISbitResolution-1 downto 0);				
-				end if;
-			end if;
-			if bitcount<IISbitResolution then
+      --Set IISdata 0
+    elsif rising_edge(bitclk) then
+      --Put data from IISdata to byteStream
+    elsif rising_edge(clk) then
+      if reset = '0' then
+	--Set IISdata 0
+	dacdat <= '0';
+      elsif rising_edge(bitclk) then
+	if daclrckPrev = not daclrck then
+	  bitcount := 0;
+	  if daclrck = '0' then
+	    dest_data_temp := std_logic_vector(byteStreamLeft)(IISbitResolution-1 downto 0);
+	  else
+	    dest_data_temp := std_logic_vector(byteStreamRight)(IISbitResolution-1 downto 0);				
+	  end if;
+	end if;
+     if bitcount<IISbitResolution then
 				if daclrck = '0' then
 					dacdat <= dest_data_temp(bitcount);
 				else
@@ -54,7 +51,7 @@ begin
 				bitcount := bitcount+1;
 			end if;
 			daclrckPrev := daclrck;
-		end if;
-	 end if;
+end if;
+    end if;
   end process;
 end stream;
